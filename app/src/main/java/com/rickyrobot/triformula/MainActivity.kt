1e0b1ee9577d62337d5263cdd40db56e1f45676b
Ricardo Ruelas <ricardo.ruelasv3@gmail.com>
2023-10-28 19:39:26 -0600

    Nuevo: Se agrega Spinner con opciones de prueba a la main activity.

--- app/src/main/java/com/rickyrobot/triformula/MainActivity.kt
+++ app/src/main/java/com/rickyrobot/triformula/MainActivity.kt
@@ -2,7 +2,10 @@ package com.rickyrobot.triformula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.rickyrobot.triformula.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val spinner = binding.spinner
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.formulas,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+
     }
 }
